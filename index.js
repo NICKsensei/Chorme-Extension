aa0d395724a42bfbf61855a835c3899a4ce4ff65
NICKsensei <80743409+NICKsensei@users.noreply.github.com>
2022-10-22 20:47:27 +0600

    innerHTML

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-let inputBtn = document.getElementById("input-btn");
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function () {
-  console.log("Saved");
+  myLeads.push(inputEl.value);
+  console.log(myLeads);
 });
+
+// Log out the items in the myLeads array using a for loop
+for (let i = 0; i < myLeads.length; i++) {
+  console.log(myLeads[i]);
+  ulEl.innerHTML += "<li>" + myLeads[i] + " " + "</li>";
+}
